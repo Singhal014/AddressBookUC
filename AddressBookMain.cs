10ac9479c79fc1a14db748d23503980ca31de966
singhal <prankulsinghal014@gmail.com>
2025-01-13 00:17:16 +0530

    [prankul] add : uc 4

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -6,10 +6,31 @@ namespace AddressBookUc
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Address Book System");
+
+
+        private static void ManageAddressBook(string bookName)
+        {
+            while (true)
+            {
+                Console.WriteLine($"\nManaging Address Book: {bookName}");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Display Contacts");
+                Console.WriteLine("3. Edit Contact");
+                Console.WriteLine("4. Delete Contact");
+                Console.WriteLine("5. Back to Main Menu");
+                Console.Write("Enter your choice: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
                         Console.WriteLine("Enter Contact Details:");
                         Console.Write("First Name: ");
                         string firstName = Console.ReadLine();
-                        Console.Write("Last Name: "                        string lastName = Console.ReadLine();
+                        Console.Write("Last Name: ");
+                        string lastName = Console.ReadLine();
                         Console.Write("Address: ");
                         string address = Console.ReadLine();
                         Console.Write("City: ");
@@ -23,7 +44,28 @@ namespace AddressBookUc
                         Console.Write("Email: ");
                         string email = Console.ReadLine();
                         Contact.AddContact(bookName, firstName, lastName, address, city, state, zip, phoneNumber, email);
+                        break;
 
+                    case "2":
+                        Contact.DisplayContacts(bookName);
+                        break;
+
+                    case "3":
+                        Contact.EditContact(bookName);
+                        break;
+
+                    case "4":
+                        Contact.DeleteContact(bookName);
+                        break;
+
+                    case "5":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
